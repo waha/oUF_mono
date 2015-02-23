2f0ef78f4449d5b64128ef4e4b566135458e3ac3
Richard Olsson <richard.olsson@gmail.com>
2015-02-23 08:47:14 +0100

    Initial commit from Mono UI v12.3.5

---
+++ cfg.lua
@@ -0,0 +1,194 @@
+  local addon, ns = ...
+  local cfg = CreateFrame("Frame")
+
+  cfg.oUF = { 
+    -- CONFIG
+	settings = {		-- general settings
+		ReverseHPbars = false,				-- fill health bars from right to left instead of standard left -> right direction 
+		health_spark = true,				-- add spark to the health bar
+		class_color_power = false,			-- class color power bar
+		show_class = false,					-- display additional class indication @ unit frames
+		pvp_flag = false,					-- enables PVP flag next to player's name
+		playerauras = "DEBUFFS",  			-- small aura frame for player, available options: "BUFFS", "DEBUFFS", "AURAS", "NONE"
+		auratimers = {
+			["enable"] = true,				-- aura timers
+			["font_size"] = 11,				-- aura timer font size
+		},
+		ghost_target = true, 				-- fake target bars that spawn if you don't have anything targeted
+		raid_mark = {
+			["enable"] = true,				-- enable raid marks
+			["alpha"] = 0.6,				-- raid mark opacity
+			["size"] = 16,					-- raid mark size
+		},
+		CombatFeedback = false,				-- enables CombatFeedback on player and target unit frames
+		Portrait = true,					-- enables 3d portraits on player and target unit frames
+		ClassBars = {
+			["enable"] = true,				-- enable class specific bars (totems, runes etc.)
+			["undock"] = false,				-- set a custom position for this element
+			["position"] = {"CENTER",UIParent,"BOTTOM",0,250},
+		},
+		AltPowerBar = {
+			["enable"] = true,				-- let oUF handle alternative powerbar
+			["undock"] = true,				-- set custom position for it
+			["position"] = {"BOTTOM", "UIParent", "BOTTOM", 0, 275},
+		},
+		click2focus = {					
+			["enable"] = true,				-- set focus macro on modified frame click (may cause taint)
+			["key"] = "Shift",				-- modifier key to focus
+		},
+	},
+	frames = {			-- unit frames settings
+		player = {						-- Player's frame
+			["position"] = {"TOP","UIParent","BOTTOM", -274, 273},		
+			["width"] = 229,
+			["height"] = 24,
+			["scale"] = 1,
+		},
+		target = {						-- Target's frame
+			["position"] = {"TOP","UIParent","BOTTOM", 274, 273},	
+			["width"] = 229,
+			["height"] = 24,
+			["scale"] = 1,
+		},
+		tot = {
+			["enable"] = true,			-- Target of Target
+			["position"] = {"TOPRIGHT", "oUF_monoTargetFrame", "BOTTOMRIGHT", 0, -42},
+			["width"] = 123,
+			["height"] = 20,
+			["scale"] = 0.9,
+		},
+		pet = {
+			["enable"] = true,			-- Player's pet
+			["position"] = {"TOPLEFT", "oUF_monoPlayerFrame", "BOTTOMLEFT", 0, -42},
+			["width"] = 123,
+			["height"] = 20,
+			["scale"] = 0.9,
+		},
+		focus = {
+			["enable"] = true,			-- Focus target + target of focus target
+			["position"] = {"TOPRIGHT", "oUF_monoPlayerFrame", "BOTTOMRIGHT", 0, -42},
+			["target_position"] = {"TOPLEFT", "oUF_monoTargetFrame", "BOTTOMLEFT", 0, -42},
+			["width"] = 123,
+			["height"] = 20,
+			["scale"] = 0.9,
+		},
+		party = {
+			["enable"] = true,			-- Party frames
+			["position"] = {"BOTTOMLEFT", "UIParent", "BOTTOMLEFT", 120, 362},
+			["spacing"] = 40,			-- spacing between party units
+			["width"] = 196,
+			["height"] = 22,
+			["scale"] = 1,
+		},
+		arena_boss = {
+			["enable_arena"] = true,	-- Boss & Arena frames
+			["enable_boss"] = true,
+			["position"] = {"BOTTOMRIGHT", "UIParent", "BOTTOMRIGHT", -120, 362},
+			["spacing"] = 56,			-- spacing between units
+			["width"] = 196,
+			["height"] = 22,
+			["scale"] = 1,
+		},
+		raid = {						-- Raid frames configuration
+			["enable"] = true,			
+			["position"] = {"TOPLEFT", "UIParent", "BOTTOM", -156, 177},
+			
+			["party"] = false, 								-- show party as 5 men raid group
+			["raid5"] = false, 								-- show raid frame for 5 (or less) men raid group
+			["raid40"] = false, 								-- allow raid frames to change their size if there are more than 30 players in the group
+			["raid_menu"] = false,							-- enable/disable right-click menu for raid frames
+			main_tank = { 
+				["enable"] = true,														-- enable Main tank frames
+				["position"] = {"BOTTOMLEFT", "UIParent", "BOTTOMRIGHT", -163, 233},	-- MTs frame position
+				["scale"] = 1.5, 														-- MT size relatively to unit size
+			},
+			["width"] = 59, 								-- raid unit width
+			["height"] = 27, 								-- raid unit height
+			["spacing"] = 4, 								-- spacing between units
+			["name_length"] = 4, 							-- number of letters to display
+			["font_size"] = 12, 							-- font size for names / hp values
+			["orientation"] = "HORIZONTAL", 				-- hp/mp bar direction
+			["focus_color"] = {.8, .8, .2, .7}, 			-- focus border color
+			["DisableRaidManager"] = true, 					-- disable default compact Raid Manager button
+			["update_time"] = .25, 							-- Enhances update rate for indicators (can be cpu intensive with lower values)
+			debuff = {	
+				["size"] = 11, 								-- debuff icon size
+				["timer"] = 5,								-- enable/disable timer for raid debuffs 
+			}, 
+			indicators = { 							
+				["enable"] = true,							-- enable/disable raid frames indicators
+				["size"] = 5, 								-- square indicator size
+				["counter_size"] = 11, 						-- bottom right corner counter size
+			},
+			icons = {
+				["size"] = 10, 								-- informative icon size (aka RL,ML,A etc.)
+				["leader"] = true,							-- toggle leader/assistant/master looter icons visibility on raid units
+				["raid_mark"] = true, 						-- toggle raid mark visibility on raid units
+				["ready_check"] = true, 					-- ready check icon
+				["role"] = true, 							-- toggle group role indication on/off
+			},
+			powerbar = {
+				["enable"] = true, 							-- toggle display of tiny power bars on raid frames
+				["size"] = 0.09, 							-- power bar thickness relatively to unit size
+			},
+			healbar = { 								
+				["enable"] = true,							-- enable healing prediction bar
+				["healalpha"] = 0.25, 						-- heal prediction bar alpha
+				["healoverflow"] = 1.2, 						-- overhealing display (1 = disabled, may take values higher than 1)
+				["healtext"] = false, 						-- show/hide heal prediction text
+			},
+			["absorbtext"] = false,							-- display total absorbs text
+		},
+	},
+	castbar = {			-- cast bars settings
+		color = {
+			["normal"] = {.3,.45,.65},
+			["uninterruptable"] = {1,.49,0},
+		},
+		player = {
+			["enable"] = true,								-- enable cast bar for player frame
+			["undock"] = false,								-- detach cast bar from frame
+			["position"] = {"CENTER",UIParent,"BOTTOM",10,320}, -- custom castbar position if undocked
+			["width"] = 210,								-- cast bar width if undocked
+			["height"] = 17, 								-- cast bar height if undocked
+		},
+		target = {
+			["enable"] = true,
+			["undock"] = false,
+			["position"] = {"CENTER",UIParent,"BOTTOM",10,360},	
+			["width"] = 210,
+			["height"] = 17,
+		},
+		focus = {
+			["enable"] = true,
+			["undock"] = true,
+			["position"] = {"CENTER",UIParent,"BOTTOM",10,470},
+			["width"] = 280,
+			["height"] = 17,
+		},
+	},
+	-- MEDIA
+	media = {
+		statusbar = "Interface\\Addons\\oUF_mono\\media\\statusbar",
+		auratex = "Interface\\Addons\\oUF_mono\\media\\iconborder",
+		font = "Interface\\Addons\\oUF_mono\\media\\font.ttf",
+		backdrop_texture = "Interface\\Addons\\oUF_mono\\media\\backdrop",
+		backdrop_edge_texture = "Interface\\Addons\\oUF_mono\\media\\backdrop_edge",
+		
+		aurafont = "Interface\\Addons\\oUF_mono\\media\\auras.ttf",
+		debuffborder = "Interface\\Addons\\oUF_mono\\media\\iconborder",
+		highlightTex = "Interface\\Buttons\\WHITE8x8",
+	},
+  }
+  
+  -- my config 
+  if GetUnitName("player") == "Strigoy" or GetUnitName("player") == "Strig" then
+	cfg.oUF.settings.playerauras = "DEBUFFS"
+	cfg.oUF.frames.party.position = {"BOTTOMRIGHT", "oUF_monoPlayerFrame", "TOPLEFT", -50, 70}
+	cfg.oUF.frames.arena_boss.position = {"BOTTOMLEFT", "oUF_monoTargetFrame", "TOPRIGHT", 50, 70}
+	cfg.oUF.frames.arena_boss.position = {"BOTTOMLEFT", "oUF_monoTargetFrame", "TOPRIGHT", 50, 70}
+	cfg.oUF.frames.raid.main_tank.position = {"TOPLEFT", "UIParent", "BOTTOM", 167, 177}
+	cfg.oUF.frames.raid.main_tank.scale = 1.1
+  end
+  -- HANDOVER
+  ns.cfg = cfg
---
+++ raid_debuffs.lua
@@ -0,0 +1,306 @@
+﻿-- /dump GetMouseFocus() -- from dungeon journal
+-- /dump EJ_GetInstanceInfo(ID)
+
+local L = {
+	["Dragon Soul"] = 824,
+	["Mogu'shan Vaults"] = 896, -- instanceID 317,
+	["Heart of Fear"] = 897, --330, 
+	["Terrace of Endless Spring"] = 886, --320,
+	["Pandaria"] = 809, --322,
+	["Throne of Thunder"] = 930, -- 362
+	["Siege of Orgrimmar"] = 953, -- 369
+}
+
+raid_debuffs = {
+    ascending = {
+        --[GetSpellInfo(92956)] = true, -- Wrack
+		--[6788] = true, -- Weakened Soul
+    },
+	debuffs = {
+		-- Any Zone
+		--["Weakened Soul"] = 12, -- testing purpposes only
+		--[6788] = 12, -- Weakened Soul
+		--["Sated"] = 12, -- testing purpposes only
+		[GetSpellInfo(12294)] = 8, -- Mortal Strike 
+		[GetSpellInfo(82928)] = 8, -- Aimed Shot 
+		[GetSpellInfo(2139)] = 10, -- Counterspell 
+		[GetSpellInfo(2094)] = 10, -- Blind 
+		[GetSpellInfo(33786)] = 10, -- Cyclone 
+		[GetSpellInfo(118)] = 7, -- Polymorph 
+		[GetSpellInfo(339)] = 7, -- Entangling Roots 
+		[GetSpellInfo(1499)] = 7, -- Freezing Trap 
+		[GetSpellInfo(25809)] = 6, -- Crippling Poison 
+		[GetSpellInfo(1715)] = 5, -- Hamstring 
+		[GetSpellInfo(5782)] = 3, -- Fear 
+		[GetSpellInfo(8122)] = 3, -- Psychic Scream 
+		[GetSpellInfo(5484)] = 3 -- Howl of Terror 
+	},
+	instances = {
+		[L["Siege of Orgrimmar"]] = {
+			-- Immerseus
+			[GetSpellInfo(143436)] = 6,    -- Corrosive Blast (Tank switch)
+			[GetSpellInfo(143459)] = 5,    -- Sha Residue
+			-- The Fallen Protectors
+			[GetSpellInfo(143198)] = 5,    -- Garrote (DoT)
+			[GetSpellInfo(143434)] = 6,    -- Shadow Word: Bane (Dispel)
+			[GetSpellInfo(147383)] = 5,    -- Debilitation (Heroic)
+			-- Norushen
+			[GetSpellInfo(146124)] = 6,    -- Self Doubt (Tank switch)
+			[GetSpellInfo(144514)] = 5,    -- Lingering Corruption (Dispel)
+			-- Sha of Pride
+			[GetSpellInfo(144358)] = 6,    -- Wounded Pride (Tank switch)
+			[GetSpellInfo(144351)] = 5,    -- Mark of Arrogance (Dispel)
+			[GetSpellInfo(146594)] = 5,    -- Gift of the Titans
+			[GetSpellInfo(147207)] = 5,    -- Weakened Resolve (Heroic)
+			-- Galakras
+			[GetSpellInfo(146765)] = 5,    -- Flame Arrows (DoT)
+			[GetSpellInfo(146902)] = 5,    -- Poison-Tipped Blades (Poison stacks)
+			-- Iron Juggernaut
+			[GetSpellInfo(144467)] = 6,    -- Ignite Armor (Tank stacks)
+			[GetSpellInfo(144459)] = 5,    -- Laser Burn (DoT)
+			-- Kor'kron Dark Shaman
+			[GetSpellInfo(144215)] = 5,    -- Froststorm Strike (Tank stacks)
+			[GetSpellInfo(144089)] = 5,    -- Toxic Mist (DoT)
+			[GetSpellInfo(144330)] = 5,    -- Iron Prison (Heroic)
+			-- General Nazgrim
+			[GetSpellInfo(143494)] = 5,    -- Sundering Blow (Tank stacks)
+			[GetSpellInfo(143638)] = 5,    -- Bonecracker (DoT)
+			[GetSpellInfo(143431)] = 5,    -- Magistrike (Dispel)
+			-- Malkorok
+			[GetSpellInfo(142990)] = 5,    -- Fatal Strike (Tank stacks)
+			[GetSpellInfo(142913)] = 5,    -- Displaced Energy (Dispel)
+			-- Spoils of Pandaria
+			[GetSpellInfo(145218)] = 5,    -- Harden Flesh (Dispel)
+			[GetSpellInfo(146235)] = 5,    -- Breath of Fire (Dispel)
+			-- Thok the Bloodthirsty
+			[GetSpellInfo(143766)] = 5,    -- Panic (Tank stacks)
+			[GetSpellInfo(143780)] = 5,    -- Acid Breath (Tank stacks)
+			[GetSpellInfo(143773)] = 5,    -- Freezing Breath (Tank Stacks)
+			[GetSpellInfo(143800)] = 5,    -- Icy Blood (Random Stacks)
+			[GetSpellInfo(143767)] = 5,    -- Scorching Breath (Tank Stacks)
+			[GetSpellInfo(143791)] = 5,    -- Corrosive Blood (Dispel)
+			-- Siegecrafter Blackfuse
+			[GetSpellInfo(143385)] = 5,    -- Electrostatic Charge (Tank stacks)
+			[GetSpellInfo(144236)] = 5,    -- Pattern Recognition
+			-- Paragons of the Klaxxi
+			[GetSpellInfo(142929)] = 5,    -- Tenderizing Strikes (Tank stacks)
+			[GetSpellInfo(143275)] = 5,    -- Hewn (Tank stacks)
+			[GetSpellInfo(143279)] = 5,    -- Genetic Alteration (Tank stacks)
+			[GetSpellInfo(143974)] = 5,    -- Shield Bash (Tank stun)
+			[GetSpellInfo(142948)] = 5,    -- Aim
+			-- Garrosh Hellscream
+			[GetSpellInfo(145183)] = 5,    -- Gripping Despair (Tank stacks)
+			[GetSpellInfo(145195)] = 5,    -- Empowered Gripping Despair (Tank stacks) 
+		},
+	
+		[L["Throne of Thunder"]] = {
+		-- Throne of Thunder
+			-- Jin'rokh the Breaker
+			[GetSpellInfo(138349)] = 5,	-- Static Wound (Tank stacks)
+			[GetSpellInfo(138349)] = 5,	-- Thundering Throw (Tank stun)
+			[GetSpellInfo(138732)] = 8,	-- Ionization (Heroic-Dispel)
+			[GetSpellInfo(137422)] = 7,	-- Focused Lightning (Fixated - Kiting)
+			[GetSpellInfo(138006)] = 6,	-- Electrified Waters (Pool)
+			-- Horridon
+			[GetSpellInfo(136767)] = 5,	-- Triple Puncture (Tank stacks)
+			[GetSpellInfo(136654)] = 7,	-- Rending Charge (DoT)
+			[GetSpellInfo(136708)] = 8,	-- Stone Gaze (Stun)
+			[GetSpellInfo(136723)] = 7,	-- Sand Trap (Pool)
+			[GetSpellInfo(136710)] = 7,	-- Deadly Plague (Dispel)
+			[GetSpellInfo(136719)] = 7,	-- Blazing Sunlight (Dispel)
+			[GetSpellInfo(136587)] = 7,	-- Venom Bolt Volley (Dispel)
+			[GetSpellInfo(136512)] = 8,	-- Hex of Confusion (Dispel)
+			-- Council of Elders
+			[GetSpellInfo(136903)] = 5,	-- Frigid Assault (Tank stacks)
+			[GetSpellInfo(136922)] = 8,	-- Frostbite (DoT + stun)
+			[GetSpellInfo(136857)] = 7,	-- Entrapped (Dispel)
+			[GetSpellInfo(137359)] = 7,	-- Marked Soul (Fixated - Kiting)
+			[GetSpellInfo(137641)] = 7,	-- Soul Fragment (Heroic)
+			[GetSpellInfo(137650)] = 6,	-- Shadowed Soul
+			-- Tortos
+			[GetSpellInfo(136753)] = 5,	-- Slashing Talons (Tank DoT)
+			[GetSpellInfo(137633)] = 7,	-- Crystal Shell (Heroic)
+			[GetSpellInfo(140701)] = 8, -- Crystal Shell: Full Capacity! (Heroic)
+			-- Megaera
+			[GetSpellInfo(137731)] = 5,	-- Ignite Flesh (Tank DoT)
+			[GetSpellInfo(139843)] = 5,	-- Arctic Freeze (Tank Stun)
+			[GetSpellInfo(139840)] = 5,	-- Rot Armor (Tank stacks)
+			[GetSpellInfo(134391)] = 8,	-- Cinder (DoT Dispell)
+			[GetSpellInfo(139857)] = 7,	-- Torrent of Ice (Fixated - Kiting)
+			[GetSpellInfo(140179)] = 7,	-- Suppression (Heroic Dispell)
+			[GetSpellInfo(139909)] = 7,	-- Icy Ground (Pool)
+			-- Ji-Kun
+			[GetSpellInfo(140092)] = 5,	-- Infected Talons (Tank DoT)
+			[GetSpellInfo(134256)] = 7,	-- Slimed (DoT)
+			-- Durumu the Forgotten
+			[GetSpellInfo(133768)] = 4,	-- Arterial Cut (Tank stacks)
+			[GetSpellInfo(133767)] = 5,	-- Serious Wound (Tank only)
+			[GetSpellInfo(133798)] = 7,	-- Life Drain (Stun)
+			[GetSpellInfo(133597)] = 7,	-- Dark Parasite (Heroic Dispel)
+			-- Primordius
+			[GetSpellInfo(136050)] = 5,	-- Malformed Blood (Tank stacks)
+			[GetSpellInfo(136228)] = 8,	-- Volatile Pathogen (DoT)
+			[GetSpellInfo(137000)] = 7, -- Black Blood
+			-- Dark Animus
+			[GetSpellInfo(138569)] = 5,	-- Explosive Slam (Tank stacks)
+			[GetSpellInfo(138659)] = 7,	-- Touch of the Animus (DoT)
+			[GetSpellInfo(138486)] = 8, -- Crimson Wake Target Debuff
+			[GetSpellInfo(138609)] = 8, -- Matter Swap
+			[GetSpellInfo(136962)] = 7, -- Anima Ring
+			-- Iron Qon
+			[GetSpellInfo(134691)] = 5,	-- Impale (Tank stacks)
+			[GetSpellInfo(134628)] = 7,	-- Unleashed Flame (Damage staks)
+			[GetSpellInfo(136192)] = 7,	-- Lightning Storm (Stun)
+			[GetSpellInfo(135145)] = 7,	-- Freeze (Stun+aoe damage)
+			[GetSpellInfo(137668)] = 8, -- Burning Cinders
+			[GetSpellInfo(137669)] = 8, -- Arcing Lightning
+			[GetSpellInfo(135145)] = 8, -- Freeze
+			-- Twin Consorts
+			[GetSpellInfo(137408)] = 5,	-- Fan of Flames (Tank stacks)
+			[GetSpellInfo(136722)] = 7,	-- Slumber Spores (Dispel)
+			[GetSpellInfo(137341)] = 7,	-- Beast of Nightmares (Fixate - DON"T HEAL THE TARGET SO MUCH)
+			[GetSpellInfo(137360)] = 7,	-- Corrupted Healing (Oh, did you heal the target. So prepare to massive damage, haha)
+			[GetSpellInfo(137440)] = 8, -- Icy Shadows
+			-- Lei Shen
+			--[GetSpellInfo(135000)] = 5,	-- Decapitate (Tank only)
+			[GetSpellInfo(136478)] = 5,	-- Fusion Slash (Tank only)
+			[GetSpellInfo(136914)] = 5,	-- Electrical Shock (Tank only)
+			[GetSpellInfo(135695)] = 7,	-- Static Shock (Damage Split)
+			[GetSpellInfo(136295)] = 8, -- Overcharge
+			[GetSpellInfo(139011)] = 9, -- Helm of Command
+			-- Ra-den
+		},
+	
+		[L["Mogu'shan Vaults"]] = {
+			-- The Stone Guard
+			[GetSpellInfo(116281)] = 7, -- Cobalt Mine Blast, Magic root
+
+			--Feng the Accursed
+			[GetSpellInfo(116040)] = 7, -- Epicenter, roomwide aoe.
+			[GetSpellInfo(116784)] = 7, -- Wildfire Spark, Debuff that explodes leaving fire on the ground after 5 sec.
+			[GetSpellInfo(116374)] = 7, -- Lightning Charge, Stun debuff.
+			[GetSpellInfo(116417)] = 7, -- Arcane Resonance, aoe-people-around-you-debuff.
+			[GetSpellInfo(116942)] = 7, -- Flaming Spear, fire damage dot.
+
+			-- Gara'jal the Spiritbinder
+			[GetSpellInfo(122151)] = 7,	-- Voodoo Doll, shared damage with the tank.
+			[GetSpellInfo(116161)] = 7,	-- Crossed Over, people in the spirit world.
+
+			-- The Spirit Kings
+			[GetSpellInfo(117708)] = 7, -- Meddening Shout, The mind control debuff.
+			[GetSpellInfo(118303)] = 7, -- Fixate, the once targeted by the shadows.
+			[GetSpellInfo(118048)] = 7, -- Pillaged, the healing/Armor/damage debuff.
+			[GetSpellInfo(118135)] = 7, -- Pinned Down, Najentus spine 2.0
+
+			--Elegon
+			--[GetSpellInfo(117878)] = 7, -- Overcharged, the stacking increased damage taken debuff.	
+			--[GetSpellInfo(117870)] = 7, -- Touch of the Titans, the debuff everybody gets increasing damage done and healing taken.
+			[GetSpellInfo(117949)] = 7, -- Closed Circuit, Magic Healing debuff.
+
+			--Will of the Emperor
+			[GetSpellInfo(116969)] = 7, -- Stomp, Stun from the bosses.
+			[GetSpellInfo(116835)] = 7, -- Devestating Arc, Armor debuff from the boss.
+			[GetSpellInfo(116778)] = 7, -- Focused Defense, Fixate from the Emperors Courage.
+			[GetSpellInfo(117485)] = 7, -- Impending Thrust, Stacking slow from the Emperors Courage.
+			[GetSpellInfo(116525)] = 7, -- Focused Assault, Fixate from the Emperors Rage
+			[GetSpellInfo(116550)] = 7, -- Energizing Smash, Knockdown from the Emperors Strength
+		},
+		[L["Heart of Fear"]] = {
+			-- Imperial Vizier Zor'lok
+			[GetSpellInfo(122761)] = 7, -- Exhale, The person targeted for Exhale. 
+			[GetSpellInfo(123812)] = 7, -- Pheromones of Zeal, the gas in the middle of the room.
+			[GetSpellInfo(122706)] = 7, -- Noise Cancelling, The "safe zone" from the roomwide aoe.
+			[GetSpellInfo(122740)] = 7, -- Convert, The mindcontrol Debuff.
+
+			-- Blade Lord Ta'yak
+			[GetSpellInfo(123180)] = 7, -- Wind Step, Bleeding Debuff from stealth.
+			[GetSpellInfo(123474)] = 7, -- Overwhelming Assault, stacking tank swap debuff. 
+
+			-- Garalon
+			[GetSpellInfo(122774)] = 7, -- Crush, stun from the crush ability.
+			--[GetSpellInfo(123426)] = 7, -- Weak Points, Increased damage done to one leg.
+			--[GetSpellInfo(123428)] = 7, -- Weak Points, Increased damage to another leg.
+			--[GetSpellInfo(123423)] = 7, -- Weak Points, Increased damage to another leg.
+			--[GetSpellInfo(123235)] = 7, -- Weak Points, Increased damage to another leg.
+			[GetSpellInfo(122835)] = 7, -- Pheromones, The buff indicating who is carrying the pheramone.
+			[GetSpellInfo(123081)] = 8, -- Punchency, The stacking debuff causing the raid damage.
+
+			--Wind Lord Mel'jarak
+			[GetSpellInfo(122055)] = 7, -- Residue, The debuff after breaking a prsion preventing further breaking.
+			[GetSpellInfo(121885)] = 7, -- Amber Prison, The stun that somebody has to click off.
+			[GetSpellInfo(121881)] = 7, -- Amber Prison, not sure what the differance is but both were used.
+			[GetSpellInfo(122125)] = 7, -- Corrosive Resin pool, the **** on the floor your not supposed to stand in.
+			[GetSpellInfo(122064)] = 7, -- Corrosive Resin, the dot you clear by moving/jumping.
+
+			-- Amber-Shaper Un'sok 
+			[GetSpellInfo(122370)] = 7, -- Reshape Life, the transformation ala putricide.
+			[GetSpellInfo(122784)] = 7, -- Reshape Life, Both were used.
+			[GetSpellInfo(124802)] = 7, -- The transformed players increase damage taken cooldown.
+			[GetSpellInfo(122395)] = 7, -- Struggle for Control, the self stun used to interupt the channel.
+			[GetSpellInfo(122457)] = 7, -- Rough Landing, The stun from being tossed and being hit by the toss from the add in Phase 2.
+			[GetSpellInfo(121949)] = 7, -- Parasitic Growth, the dot that scales with healing taken.
+			
+			-- Empress
+			[GetSpellInfo(125390)] = 7, -- Fixate
+			[GetSpellInfo(123184)] = 7, -- Dissonance Field
+			[GetSpellInfo(123788)] = 6, -- Cry of terror
+			[GetSpellInfo(126122)] = 7, -- Corrupted Dissonance Field
+			
+		},
+		[L["Terrace of Endless Spring"]] = {
+			--Protectors Of the Endless
+			--[GetSpellInfo(117519)] = 7, -- Touch of Sha, Dot that lasts untill Kaolan is defeated.
+			--[GetSpellInfo(117235)] = 7, -- Purified, haste buff gained by killing mist and being in range.
+			--[GetSpellInfo(117353)] = 7, -- Overwhelming Corruption, stacking raidwide softenrage debuff.
+			[GetSpellInfo(118091)] = 7, -- Defiled Ground, Increased damage taken from Defiled ground debuff.
+			[GetSpellInfo(117436)] = 12, -- Lightning Prison, Magic stun.
+
+			--Tsulong
+			[GetSpellInfo(122768)] = 7, -- Dread Shadows, Stacking raid damage debuff (ragnaros superheated style) 
+			[GetSpellInfo(122789)] = 7, -- Sunbeam, standing in the sunbeam, used to clear dread shadows.
+			[GetSpellInfo(122858)] = 7, -- Bathed in Light, 500% increased healing done debuff.
+			[GetSpellInfo(122752)] = 7, -- Shadow Breath, increased shadow breath damage debuff.
+			[GetSpellInfo(123011)] = 7, -- Terrorize, Magical dot dealing % health.
+			[GetSpellInfo(123036)] = 7, -- Fright, 2 second fear.
+			[GetSpellInfo(122777)] = 7, -- Nightmares, 3 second fear.
+
+			--Lei Shi
+			[GetSpellInfo(123121)] = 7, -- Spray, Stacking frost damage taken debuff.
+
+			--Sha of Fear
+			[GetSpellInfo(129147)] = 7, -- Ominous Cackle, Debuff that sends players to the outer platforms.
+			[GetSpellInfo(119086)] = 7, -- Penetrating Bolt, Increased Shadow damage debuff.
+			[GetSpellInfo(119775)] = 7, -- Reaching Attack, Increased Shadow damage debuff.
+			[GetSpellInfo(119985)] = 7, -- Dread Spray, stacking magic debuff, fears at 2 stacks.
+			[GetSpellInfo(119983)] = 7, -- Dread Spray, is also used.
+			[GetSpellInfo(119414)] = 7, -- Breath of Fear, Fear+Massiv damage.
+			
+			[GetSpellInfo(120629)] = 10, -- Huddle in Terror
+		},
+		[L["Dragon Soul"]] = {
+			-- Morchok
+			[GetSpellInfo(103687)] = 7, -- Crush Armor
+			-- Warlord Zon'ozz
+			[GetSpellInfo(103434)] = 8, -- Disrupting Shadows
+			-- some shit boss with blobs
+			[GetSpellInfo(104849)] = 8, -- Void Bolt
+			[109389] = 12, 				-- Deep Corruption stacks (108220)
+			[103628] = 12,				-- Deep Corruption stacks
+			-- Hagara the Stormbinder
+			[GetSpellInfo(109325)] = 7, -- Frostflake, HC
+			[GetSpellInfo(104451)] = 7, -- Ice Tomb
+			[GetSpellInfo(105313)] = 7, -- Ice Lance
+			[GetSpellInfo(105289)] = 7, -- Shattered Ice
+			-- Blackhorn
+			-- Ultraxion
+			[GetSpellInfo(109075)] = 8, -- Fading Light
+			-- Spine of Deathwing
+			[GetSpellInfo(109379)] = 7, -- Searing Plasma
+			[GetSpellInfo(105490)] = 8, -- Fiery Grip
+			-- Madness 
+			[GetSpellInfo(105841)] = 7,  -- Degenerative Bite
+			[GetSpellInfo(105445)] = 8,  -- Blistering Heat
+			[GetSpellInfo(106444)] = 9,  -- Impale
+		},
+	},
+}
